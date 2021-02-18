9d87b7bd81373ed0d1de05b098a2b1cf920a2549
rohith0696 <60017598+rohith0696@users.noreply.github.com>
2021-02-17 22:31:38 -0600

    added code for the locations

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,35 @@
 // import * as randLoc from './location.js';
+import locationsArray from '../init-locations.js';
+
+let locationElement = document.getElementById("bgrtwo");
+let locationElement2 = document.getElementById("bgrone");
+
+
+
+window.addEventListener('load', main);
+
+locationElement.addEventListener('click', locationHandler);
+locationElement.addEventListener('touch', locationHandler);
+locationElement2.addEventListener('click', colorFunction1);
+locationElement2.addEventListener('touch', colorFunction1);
+
+function main() {
+    console.log('Page is fully loaded');
+}
+
 
 
 function colorFunction1() {
-    const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", ];
-    const random = Math.floor(Math.random() * values.length);
-    let value = values[random];
-    document.getElementById("bgrone").style.backgroundColor = "#7aeb7a";
-    document.getElementById("lbl").innerHTML = value;
-    let utterance = new SpeechSynthesisUtterance(`     You have picked the card of color Light Green of thevalue     ${value}`);
+    document.getElementById("targetloc").innerHTML = "The Treasure is in the location ";
+    document.getElementById("lbl").innerHTML = targetLoc.Name;
+    document.getElementById("device-lat1").innerHTML = targetLoc.latitude.toFixed(6);
+    document.getElementById("device-long1").innerHTML = targetLoc.longitude.toFixed(6);
+    let utterance = new SpeechSynthesisUtterance(`The location where the treasure is ${targetLoc.Name}`);
     speechSynthesis.speak(utterance);
-console.log(randLoc)
+
 
 }
 
-function colorFunction2() {
-    const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", ];
-    const random = Math.floor(Math.random() * values.length);
-    let value = values[random];
-    document.getElementById("bgrtwo").style.backgroundColor = "#99cfe0";
-    document.getElementById("lbl2").innerHTML = value;
-    let utterance = new SpeechSynthesisUtterance(`     You have picked the card of color Light Blue of the value     ${value}`);
-    speechSynthesis.speak(utterance);
-}
 
 async function getLocation() {
     return new Promise((resolve, reject) => {
@@ -31,20 +39,65 @@ async function getLocation() {
     });
 }
 
-let currentlat, currentlon,loc;
+let currentlat, currentlon, loc, error = true;
+let targetLoc = locationsArray[Math.floor(Math.random()*locationsArray.length)];
 
 async function locationHandler() {
     const locText = await getLocation();
-    loc = 'Your Location';
-    document.getElementById("location").innerHTML = loc;
+    // loc = 'Your current Location';
+    // document.getElementById("location").innerHTML = loc;
     currentlat = locText.coords.latitude;
     console.log(currentlat)
     document.getElementById("device-lat").innerHTML = currentlat.toFixed(6);
     currentlon = locText.coords.longitude;
     console.log(currentlon)
     document.getElementById("device-long").innerHTML = currentlon.toFixed(6);
-    // let value = currentlat+"\n"+currentlon;
-    // document.getElementById("lbl2").innerHTML = value;
+    
+    locationsArray.forEach(function (value) {
+        if (isInside(value.Latitude, value.Longitude)) {
+            document.getElementById("location").innerHTML = value.Name;
+            let utterance = new SpeechSynthesisUtterance("Congratulations!, You found location ${value.Name}");
+            speechSynthesis.speak(utterance);
+            error = false;
+        }
+    });
+
+    if(error) {
+
+        console.log("error is here")
+        document.getElementById("error-message").innerHTML = "Sorry,You're not near to the treasure";
+         let utterance = new SpeechSynthesisUtterance("Sorry,You're not near to the treasure");
+         speechSynthesis.speak(utterance);
+     } else {
+         document.getElementById("error-message").innerHTML = "";
+     }
     
 
+}
+
+
+function isInside(questLat, questLon) {
+    let distance = distanceBetweenLocations(questLat, questLon);
+    console.log("distance: " + distance);
+    if (distance < 30) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function distanceBetweenLocations(questLat, questLon) {
+    const R = 6371e3;
+    const φ1 = currentlat * Math.PI / 180;
+    const φ2 = questLat * Math.PI / 180;
+    const Δφ = (questLat - currentlat) * Math.PI / 180;
+    const Δλ = (questLon - currentlon) * Math.PI / 180;
+
+    const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+        Math.cos(φ1) * Math.cos(φ2) *
+        Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    const d = R * c;
+    return d; 
 }
